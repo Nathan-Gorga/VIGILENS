13746bc0d66536cd9c72582312de8479d9483da9
Nathan Gorga <gorga.nathan05@gmail.com>
2025-07-01 14:47:31 +0200

    function comment for initRingBuffer

--- src/RINGBUFFER/ringbuffer.c
+++ src/RINGBUFFER/ringbuffer.c
@@ -1,5 +1,15 @@
 #include "ringbuffer.h"
 
+/**
+ * @brief Initializes a ring buffer of floats with a given size.
+ *
+ * @param size The size of the buffer in floats.
+ *
+ * @return A pointer to the newly allocated ring buffer, or NULL if size is less than or equal to 0.
+ *
+ * @details The buffer is allocated with malloc, and the memory is initialized with calloc.
+ *          The write index is set to 0.
+ */
 struct ring_buffer * initRingBuffer(const size_t size){
     
     if(size <= 0) return NULL;
