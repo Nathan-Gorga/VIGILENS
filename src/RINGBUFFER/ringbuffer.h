dcfb7530dad140699c77fedb0205280f8dd99aeb
Nathan Gorga <gorga.nathan05@gmail.com>
2025-07-01 14:34:30 +0200

    ring buffer todo

--- src/RINGBUFFER/ringbuffer.h
+++ src/RINGBUFFER/ringbuffer.h
@@ -3,5 +3,17 @@
 
 #include "../globaldefinition.h"
 
+//TODO : ringbuffer struct
+
+//TODO : ringbuffer init
+
+//TODO : ringbuffer free
+
+//TODO : ringbuffer add data (need to think if we need two functions for adding just 1 value and adding a full chunk of data or combine both, would tend more towards 2 functions, would be simpler)
+
+//TODO : ringbuffer get full buffer (for internal ring buffer operation)
+
+
+
 #endif
 
