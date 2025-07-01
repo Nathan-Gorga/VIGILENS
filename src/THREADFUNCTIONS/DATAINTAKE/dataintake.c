2dcd0c996bc4f7ed1ee71c44763e4519ed822a87
Nathan Gorga <gorga.nathan05@gmail.com>
2025-07-01 13:45:54 +0200

    main todo

--- src/THREADFUNCTIONS/DATAINTAKE/dataintake.c
+++ src/THREADFUNCTIONS/DATAINTAKE/dataintake.c
@@ -4,11 +4,27 @@
 void * launchDataIntake(void * arg){
     
     dataIntake();
-    
+
     pthread_exit(NULL);
 }
 
 
 void dataIntake(void){
+    //TODO : init internal ring buffer
     
+    //TODO : send ready signal to master
+
+    //TODO : sends go signal to data stream source
+
+    //TODO : receive data 
+
+    //TODO : put data in internal ring buffer
+
+    //TODO : check if there is a leave from baseline in the ring buffer(how big should be the buffer that's checked and shoudl we send the whole buffer or bytes at a time?)
+
+    //TODO : return to data intake
+
+    //TODO : always take into account that a cancel signal may come from master
+
+    //TODO : free internal ring buffer
 }
--- src/main.c
+++ src/main.c
@@ -5,6 +5,19 @@
 
 int main(void){
     printf("Hello world\n");
+
+    //TODO : init event ring buffer
+
+    //TODO : launch data intake
+
+    //TODO : wait for ready signal from data intake
+
+    //TODO : wait for keyboard interupt to cancel data intake thread
+
+    //TODO : cancel data intake
+
+    //TODO : free event ring buffer
+
     return 0;
 }
 
