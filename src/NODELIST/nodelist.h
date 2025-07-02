42876bbba3e80cc97aff9c3e6c38dd16a5e88744
Nathan Gorga <gorga.nathan05@gmail.com>
2025-07-02 11:47:33 +0200

    nodelist todos

--- src/NODELIST/nodelist.h
+++ src/NODELIST/nodelist.h
@@ -3,5 +3,28 @@
 
 #include "../globaldefinition.h"
 
+//TODO : node struct 
+
+
+//TODO : init node 
+
+//TODO : free node 
+
+//TODO : add node to list (static)
+
+//TODO : pop node from list (static)
+
+//TODO : get most recent event from list (static)
+
+//TODO : build mutex functions that are not static to prevent race conditions
+
+
+
+
+
+
+
+
+
 #endif
 
