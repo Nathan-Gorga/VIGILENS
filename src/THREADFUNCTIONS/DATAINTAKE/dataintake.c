cba845e65266149a382679d36f9f4e5dd8433c52
Nathan Gorga <gorga.nathan05@gmail.com>
2025-07-02 18:19:42 +0200

    modified cleanup handler to free the internal ringbuffer

--- src/THREADFUNCTIONS/DATAINTAKE/dataintake.c
+++ src/THREADFUNCTIONS/DATAINTAKE/dataintake.c
@@ -1,18 +1,15 @@
 #include "dataintake.h"
 
 
-static void cleanupHandler(void * arg){//BUG : this gets called randomly on cancel it seems
+static void cleanupHandler(void * arg){
     
     (void)printf("Cancel signal received\n");    
 
-
-    //TODO : implement
-
+    freeRingBuffer((struct ring_buffer *)arg);
 
     (void)printf("Cleaned up thread\n");    
    
 }
-
 
 void * launchDataIntake(void * arg){
     
@@ -21,34 +18,30 @@ void * launchDataIntake(void * arg){
     pthread_exit(NULL);
 }
 
-
 static void dataIntake(void){
-    pthread_cleanup_push(cleanupHandler, NULL);
-
+    
     (void)printf("Thread launched succesfully\n");
-
+    
     struct ring_buffer * internal_ring_buffer = initRingBuffer(INTERNAL_RING_BUFFER_SIZE, INTERNAL_RING_BUFFER);
-
+    
     if(internal_ring_buffer == NULL) pthread_exit(NULL);
-
-
-    //TODO : send ready signal to master
+    
+    pthread_cleanup_push(cleanupHandler, internal_ring_buffer);
 
     //TODO : sends go signal to data stream source
 
     //TODO : receive data 
-
+    while(1){
+        
     //TODO : put data in internal ring buffer
 
     //TODO : check if there is a leave from baseline in the ring buffer(how big should be the buffer that's checked and shoudl we send the whole buffer or bytes at a time?)
 
     //TODO : return to data intake
 
-    //TODO : always take into account that a cancel signal may come from master
-
-    //TODO : free internal ring buffer
-
-    freeRingBuffer(internal_ring_buffer);
+    
+        pthread_testcancel();
+    }
 
     pthread_cleanup_pop(1);
 }
