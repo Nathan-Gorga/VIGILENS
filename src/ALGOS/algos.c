1a81160fed98319594b5bbd5e087071ea52a7893
Nathan Gorga <gorga.nathan05@gmail.com>
2025-07-04 10:50:07 +0200

    added simple threshold logic

--- src/ALGOS/algos.c
+++ src/ALGOS/algos.c
@@ -1,9 +1,21 @@
 #include "algos.h"
 
-bool aboveThreshold(const float data_point, const float threshold){}
+inline bool aboveThreshold(const float data_point, const float threshold){
+    return data_point > threshold;
+}
 
-bool belowThreshold(const float data_point, const float threshold){}
+inline bool belowThreshold(const float data_point, const float threshold){
+    return data_point < threshold;
+}
 
-bool isBaseline(const float data_point, const float max, const float min){}
+inline bool isBaseline(const float data_point, const float max, const float min){
+    /*
+    NOT ( TRUE OR TRUE) = FALSE 
+    NOT (TRUE OR FALSE) = FALSE
+    NOT (FALSE OR TRUE) = FALSE
+    NOT (FALSE OR FALSE) = TRUE
+    */
+    return !(aboveThreshold(data_point, max) || belowThreshold(data_point, min));
+}
 
 
--- src/ALGOS/algos.h
+++ src/ALGOS/algos.h
@@ -4,12 +4,12 @@
 #include "../globaldefinition.h"
 
 //TODO : threshold detection both maximum and minimum
-bool aboveThreshold(const float data_point, const float threshold);
+inline bool aboveThreshold(const float data_point, const float threshold);
 
-bool belowThreshold(const float data_point, const float threshold);
+inline bool belowThreshold(const float data_point, const float threshold);
 
 //TODO : baseline detection (combien both max and minimum)
-bool isBaseline(const float data_point, const float max, const float min);
+inline bool isBaseline(const float data_point, const float max, const float min);
 
 
 #endif
