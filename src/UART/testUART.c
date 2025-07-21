6b572967d6190dd755c50b97077f082155d88f53
Nathan-Gorga <gorga.nathan05@gmail.com>
2025-07-21 14:29:30 +0100

    added compiler optimizations for RPi

---
+++ src/UART/testUART.c
@@ -0,0 +1,28 @@
+#include "uart.h"
+
+
+int main() {
+    if (!beginUART()) {
+        printf("UART init failed\n");
+        return 1;
+    }
+
+    printf("succesful initialization\n");
+
+
+    float buffer[PACKET_BUFFER_SIZE * NUM_CHANNELS];
+    while (1) {
+        size_t n = getUARTData(buffer);
+        if (n > 0) {
+            for (size_t i = 0; i < n; i++) {
+                printf("%f ", buffer[i]);
+            }
+            printf("\n");
+        } else {
+	    printf("no incoming data\n");
+	}
+    }
+
+    endUART();
+    return 0;
+}
