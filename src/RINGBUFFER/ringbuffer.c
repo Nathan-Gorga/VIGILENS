d4ed7f5ab1ab649c0633980caf6ffdc24b7e87fc
Nathan-Gorga <gorga.nathan05@gmail.com>
2025-07-21 14:58:06 +0100

    optimized addFloatToRingBuffer, faster now

--- src/RINGBUFFER/ringbuffer.c
+++ src/RINGBUFFER/ringbuffer.c
@@ -238,7 +238,7 @@ void addFloatToRingBuffer(struct ring_buffer * restrict buffer, const float data
 
     buffer->memory[buffer->write] = data;// no need for mutex (internal)
 
-    writeIndexIncrement(buffer);
+    buffer->write = (buffer->write + 1) % buffer->size; //we dont use writeIndexIncrement for this one, it is only used for internal buffer and therefore we need to bypass the mutex for speed
 }
 
 
